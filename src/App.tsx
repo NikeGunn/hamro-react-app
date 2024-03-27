9729f8ae4251c1a17225429087a571cfde57e2c0
Nikhil <knewboy.nykhil@gmail.com>
2024-03-27 10:27:01 -0700

    added more content

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>React App</h1>
+      <h1>React App with vite and deploy to netlify</h1>
       <p>This is my normal website</p>
       <p>Created by someone</p>
     </>
