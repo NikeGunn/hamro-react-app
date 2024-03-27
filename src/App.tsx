2bb6ad45af73a60cd7ba14ba56aaad2d2734f20a
Nikhil <knewboy.nykhil@gmail.com>
2024-03-27 10:12:19 -0700

    created simple react app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>React App</h1>
+      <p>This is my normal website</p>
+      <p>Created by someone</p>
+    </>
+  )
+}
+
+export default App
